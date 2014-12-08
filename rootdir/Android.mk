456d4297f810997e4324222c5bcacfc2dd1cabe2
fefifofum <jorsellis@gmail.com>
2014-12-08 15:48:32 +0100

    Fix derp

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -15,7 +15,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.awifi
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/fstab.qcom
+LOCAL_SRC_FILES    := etc/fstab.awifi
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
@@ -23,7 +23,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.awifi.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.rc
+LOCAL_SRC_FILES    := etc/init.awifi.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
@@ -31,7 +31,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.awifi.usb.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.usb.rc
+LOCAL_SRC_FILES    := etc/init.awifi.usb.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
@@ -39,6 +39,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.awifi.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
+LOCAL_SRC_FILES    := etc/ueventd.awifi.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
