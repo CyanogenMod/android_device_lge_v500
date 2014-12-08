460d4a3f5a92ee2ceb467a510ef0d54ba643a810
fefifofum <jorsellis@gmail.com>
2014-12-08 13:39:38 +0100

    Update thermal configuration

--- device.mk
+++ device.mk
@@ -182,7 +182,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Thermal
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermald-8064.conf:system/etc/thermald-8064.conf
+    $(LOCAL_PATH)/configs/thermald.conf:system/etc/thermald.conf
 
 # USB
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
