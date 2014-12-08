e5cb6e2c8dd1cc8c450a7da29c7cdb3072952e28
fefifofum <jorsellis@gmail.com>
2014-12-08 04:45:17 +0100

    Cleanup keychars and keylayout files

--- device.mk
+++ device.mk
@@ -131,18 +131,14 @@ PRODUCT_COPY_FILES += \
 
 # Keychars
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keychars/apq8064-tabla-snd-card_Button_Jack.kcm:system/usr/keychars/apq8064-tabla-snd-card_Button_Jack.kcm \
     $(LOCAL_PATH)/keychars/hs_detect.kcm:system/usr/keychars/hs_detect.kcm \
-    $(LOCAL_PATH)/keychars/keypad_8064.kcm:system/usr/keychars/keypad_8064.kcm \
     $(LOCAL_PATH)/keychars/pmic8xxx_pwrkey.kcm:system/usr/keychars/pmic8xxx_pwrkey.kcm
 
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/awifi-keypad-8064.kl:system/usr/keylayout/awifi-keypad-8064.kl \
-    $(LOCAL_PATH)/keylayout/apq8064-tabla-snd-card_Button_Jack.kl:system/usr/keylayout/apq8064-tabla-snd-card_Button_Jack.kl \
     $(LOCAL_PATH)/keylayout/hs_detect.kl:system/usr/keylayout/hs_detect.kl \
-    $(LOCAL_PATH)/keylayout/pmic8xxx_pwrkey.kl:system/usr/keylayout/pmic8xxx_pwrkey.kl \
-    $(LOCAL_PATH)/keylayout/keypad_8064.kl:system/usr/keylayout/keypad_8064.kl
+    $(LOCAL_PATH)/keylayout/pmic8xxx_pwrkey.kl:system/usr/keylayout/pmic8xxx_pwrkey.kl
 
 # Lights
 PRODUCT_PACKAGES += \
