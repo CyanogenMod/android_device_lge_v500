8c7d2020e6952290afbdaca7fe514d674d235521
fefifofum <jorsellis@gmail.com>
2014-12-30 18:36:44 +0100

    Update device.mk

--- device.mk
+++ device.mk
@@ -16,32 +16,25 @@
 
 PRODUCT_CHARACTERISTICS := tablet
 
-# Overlay
+PRODUCT_TAGS += dalvik.gc.type-precise
+
 DEVICE_PACKAGE_OVERLAYS := $(LOCAL_PATH)/overlay
 
-# Screen density
 PRODUCT_AAPT_CONFIG := normal large hdpi xhdpi
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
-# Dalvik
-PRODUCT_TAGS += dalvik.gc.type-precise
 $(call inherit-product, frameworks/native/build/tablet-7in-xhdpi-2048-dalvik-heap.mk)
-
-# Kernel-headers
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
 $(call inherit-product, hardware/qcom/msm8960/msm8960.mk)
 
 # Audio
 PRODUCT_PACKAGES += \
-    alsa.msm8960 \
     audio.primary.msm8960 \
     audio.a2dp.default \
     audio.usb.default \
     audio.r_submix.default \
-    libaudio-resampler
-
-PRODUCT_PACKAGES += \
-    libqcomvoiceprocessing \
-    libqcomvoiceprocessingdescriptors
+    libaudio-resampler \
+    libqcomvoiceprocessing
     
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio_effects.conf:system/vendor/etc/audio_effects.conf \
@@ -92,20 +85,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=320 \
     persist.hwc.mdpcomp.enable=true \
     debug.egl.recordable.rgba8888=1
-
-# Configure libhwui
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.hwui.texture_cache_size=72 \
-    ro.hwui.texture_cache_flushrate=0.4 \
-    ro.hwui.layer_cache_size=48 \
-    ro.hwui.r_buffer_cache_size=8 \
-    ro.hwui.path_cache_size=32 \
-    ro.hwui.gradient_cache_size=1 \
-    ro.hwui.drop_shadow_cache_size=6 \
-    ro.hwui.text_small_cache_width=1024 \
-    ro.hwui.text_small_cache_height=1024 \
-    ro.hwui.text_large_cache_width=2048 \
-    ro.hwui.text_large_cache_height=1024
 
 # Hardware-specific features
 PRODUCT_COPY_FILES += \
