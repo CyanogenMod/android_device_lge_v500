e09356f639a437142fb1bdc98621c5c7d392474c
slebit <tillnagotannat@gmail.com>
2015-01-21 13:17:19 +0100

    Enable changelog

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,6 +43,9 @@ TARGET_KERNEL_SOURCE := kernel/lge/v500
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 lpj=67677 androidboot.hardware=awifi vmalloc=400M no_console_suspend
 TARGET_KERNEL_CONFIG := cyanogenmod_v500_defconfig
 
+# Liquid Optimizations
+LIQUID_CHANGELOG := true
+
 
 BOARD_USES_ALSA_AUDIO:= true
 BOARD_USES_LEGACY_ALSA_AUDIO:= false
