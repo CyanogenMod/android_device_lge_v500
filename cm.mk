bce96129fd41d91f07bff90aa0bbcdbf2c8cf6a3
fefifofum <jorsellis@gmail.com>
2015-02-01 19:50:46 +0100

    Fixup wrong resolution in cm.mk

--- cm.mk
+++ cm.mk
@@ -1,6 +1,6 @@
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
-TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_WIDTH := 1200
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
