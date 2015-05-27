d9f64b7478e316a501f67d8a475edc165db34e5f
fefifofum <jorsellis@gmail.com>
2015-05-27 02:03:31 +0200

    Reorganize boardconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,15 +14,6 @@
 # limitations under the License.
 #
 
-# Bootloader
-TARGET_BOOTLOADER_BOARD_NAME := AWIFI
-TARGET_BOOTLOADER_NAME := awifi
-TARGET_NO_BOOTLOADER := true
-TARGET_NO_RADIOIMAGE := true
-
-# Platform
-TARGET_BOARD_PLATFORM := msm8960
-
 # Architecture
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
@@ -30,28 +21,20 @@ TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := krait
 
-# Bionic
-MALLOC_IMPL := dlmalloc
-
-# Flags
-TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
-
-# Kernel
-TARGET_KERNEL_SOURCE := kernel/lge/awifi
-TARGET_KERNEL_CONFIG := cyanogenmod_awifi_defconfig
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.hardware=awifi vmalloc=600M
-BOARD_KERNEL_BASE := 0x80200000
-BOARD_KERNEL_PAGESIZE := 2048
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
-BOARD_CUSTOM_BOOTIMG_MK := device/lge/awifi/mkbootimg.mk
-BOARD_CUSTOM_BOOTIMG := true
-
 # Assert
 TARGET_OTA_ASSERT_DEVICE := awifi,v500
 
 # Audio
 BOARD_USES_ALSA_AUDIO:= true
+
+# Bionic
+MALLOC_IMPL := dlmalloc
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := AWIFI
+TARGET_BOOTLOADER_NAME := awifi
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
 
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
@@ -63,6 +46,10 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/awifi/bluetooth
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_SHOW_PERCENTAGE := true
 COMMON_GLOBAL_CFLAGS += -DBOARD_CHARGING_CMDLINE_NAME='"androidboot.mode"' -DBOARD_CHARGING_CMDLINE_VALUE='"chargerlogo"'
+
+# Flags
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 
 # Fonts
 EXTENDED_FONT_FOOTPRINT := true
@@ -77,6 +64,16 @@ OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
 # Hardware tunables
 BOARD_HARDWARE_CLASS := device/lge/awifi/cmhw/
 
+# Kernel
+TARGET_KERNEL_SOURCE := kernel/lge/awifi
+TARGET_KERNEL_CONFIG := cyanogenmod_awifi_defconfig
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.hardware=awifi vmalloc=600M
+BOARD_KERNEL_BASE := 0x80200000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
+BOARD_CUSTOM_BOOTIMG_MK := device/lge/awifi/mkbootimg.mk
+BOARD_CUSTOM_BOOTIMG := true
+
 # Partitions
 TARGET_USERIMAGES_USE_EXT4         := true
 TARGET_USERIMAGES_USE_F2FS         := true
@@ -86,6 +83,9 @@ BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 2248146944  # 2144M
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 12071206912 # 11512M
 BOARD_CACHEIMAGE_PARTITION_SIZE    := 838860800   # 800M
 BOARD_FLASH_BLOCK_SIZE             := 131072      # (BOARD_KERNEL_PAGESIZE * 64)
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8960
 
 # Recovery
 TARGET_RECOVERY_FSTAB := device/lge/awifi/rootdir/etc/fstab.awifi
