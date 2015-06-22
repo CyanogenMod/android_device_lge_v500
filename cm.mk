95f990d8542f4278acde524114429ee790278dd4
fefifofum <jorsellis@gmail.com>
2015-06-22 04:41:21 +0200

    Reorganize makefiles

--- cm.mk
+++ cm.mk
@@ -1,10 +1,35 @@
+#
+# Copyright 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit from hardware-specific part of the product configuration
+$(call inherit-product, device/lge/awifi/device.mk)
+$(call inherit-product-if-exists, vendor/lge/awifi/awifi-vendor.mk)
+
 # Inherit some common CM stuff
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
-# Inherit device configuration
-$(call inherit-product, device/lge/awifi/full_awifi.mk)
-
+# Device identifier. This must come after all inclusions.
+PRODUCT_DEVICE := awifi
 PRODUCT_NAME := cm_awifi
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := LG-V500
+PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=awifi_open_kr \
