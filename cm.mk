daaa7c18cc4934d5ab5e189d37f1da73c3b4c4fd
fefifofum <jorsellis@gmail.com>
2015-06-22 04:29:47 +0200

    Update fingerprint from V500-30A

--- cm.mk
+++ cm.mk
@@ -7,6 +7,6 @@ $(call inherit-product, device/lge/awifi/full_awifi.mk)
 PRODUCT_NAME := cm_awifi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=awifi_open_eu \
-    BUILD_FINGERPRINT=lge/awifi_open_eu/awifi:4.4.2/KOT49I.V50020d/.1401936573:user/release-keys \
-    PRIVATE_BUILD_DESC="awifi_open_eu-user 4.4.2 KOT49I.V50020d .1401936573 release-keys"
+    PRODUCT_NAME=awifi_open_kr \
+    BUILD_FINGERPRINT=lge/awifi_open_kr/awifi:5.0.2/LRX22G/150901527c301:user/release-keys \
+    PRIVATE_BUILD_DESC="awifi_open_kr-user 5.0.2 LRX22G 150901527c301 release-keys"
