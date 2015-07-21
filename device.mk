e80e3fa3503105543635d4ebfb26aa8cf12351d9
infected_ <zzitoz@gmail.com>
2015-07-21 06:28:58 +0100

    v500: No need to use ffmpeg xml anymore
    Change-Id: Ia31087f0c2e43c9aadb3bffee8ef445d9a67c0fb

--- device.mk
+++ device.mk
@@ -57,8 +57,7 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/media_profiles.xml:system/etc/media_profiles.xml \
 	$(LOCAL_PATH)/media_codecs.xml:system/etc/media_codecs.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
-	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
-	frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml
+	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
 # Prebuilt kl and kcm keymaps
 PRODUCT_COPY_FILES += \
