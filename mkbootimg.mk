d90b9e9ce7b3d3fdfa994e9055a90c91d6dc0d98
fefifofum <jorsellis@gmail.com>
2016-06-13 19:29:32 +0200

    Update mkbootimg.mk

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -6,7 +6,7 @@ BUMP := $(LOCAL_PATH)/bump.py
 $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES)
 	$(call pretty,"Target boot image: $@")
 	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
-	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE),raw)
+	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE))
 	$(hide) $(BUMP) $@ $@
 	@echo -e ${CL_CYN}"Made boot image: $@"${CL_RST}
 
@@ -14,5 +14,6 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES)
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(recovery_ramdisk) $(recovery_kernel)
 	$(call build-recoveryimage-target, $@)
 	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
+	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE))
 	$(hide) $(BUMP) $@ $@
 	@echo -e ${CL_CYN}"Made recovery image: $@"${CL_RST}
