fc95313b7d6db3b276dca23b7c9ba2d4907fcf46
fefifofum <jorsellis@gmail.com>
2016-11-27 23:57:27 +0100

    v500: Cleanup cmdline
    CONFIG_MSM_RTB and CONFIG_DEBUG_USER are disabled in defconfig:
    
    https://github.com/CyanogenMod/android_kernel_lge_v500/blob/aa19e35b5b17389664a2ea8825cbf952aa3b042c/arch/arm/configs/cyanogenmod_v500_defconfig#L592
    https://github.com/CyanogenMod/android_kernel_lge_v500/blob/aa19e35b5b17389664a2ea8825cbf952aa3b042c/arch/arm/configs/cyanogenmod_v500_defconfig#L3682
    
    Change-Id: Ifc139d619d8f7e6f1e15b9714dd1713fec171353

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -81,7 +81,7 @@ TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/input/lge_touch/knock_on"
 TARGET_KERNEL_SOURCE := kernel/lge/v500
 TARGET_KERNEL_CONFIG := cyanogenmod_v500_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 lpj=67677 androidboot.hardware=awifi vmalloc=400M
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 ehci-hcd.park=3 lpj=67677 androidboot.hardware=awifi vmalloc=400M
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
